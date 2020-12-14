e336a6856774ab6030c570fe1dd4f20b7e4852c6
Ryan <72888009+hhryannz@users.noreply.github.com>
2020-12-14 19:26:22 +0100

    variable options added

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const mixedMessage = () => {
+const mixedMessage = () => { //main funtion to print out results
     let greeting = 'mate';
     let question1 = 'sheep';
     let question2 = 'kangaroo';
@@ -12,3 +12,8 @@ const mixedMessage = () => {
     console.log(`What do you get when you cross a ${question1} with a ${question2}?`);
     console.log(`A big pile of ${answer}`)
 }
+
+let possibleGreetings = ["mate", "friend", "pal", "person", "human", "thing", "bucko", "kid", "you"];
+let things = ["hot dog", "computer", "hand", "T-shirt", "finger", "pen", "table", "chair", "lamp", "window", "mouse", "cat", "dog"];
+let possibleAnswer = ["fresh food", "crap", "fish and chips", "something gruesome", "poo", "gunk", "intertwined bodies" ];
+
