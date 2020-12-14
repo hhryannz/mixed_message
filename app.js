2510f63321c325192dc2bc4a4abd51d71262682e
Ryan <72888009+hhryannz@users.noreply.github.com>
2020-12-14 19:07:29 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const mixedMessage = () => {
+    let greeting = 'mate';
+    let question1 = 'sheep';
+    let question2 = 'kangaroo';
+    let answer = 'poo';
+
+
+
+
+
+    console.log(`Hello ${greeting}`);
+    console.log(`What do you get when you cross a ${question1} with a ${question2}?`);
+    console.log(`A big pile of ${answer}`)
+}
